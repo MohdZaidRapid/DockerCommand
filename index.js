6b48a9db4f6ec95b937426a9c3cf21325e1883eb
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-01-16 23:54:46 +0530

    ddeda

---
+++ index.js
@@ -0,0 +1,16 @@
+// a:docker pull imageName
+// # to pull an image from docker hub
+// #  docker pull hello-world:latest
+// b: docker images
+// # to find all images in our docker hub
+// # to search a specific  images
+// docker search imagename 
+// # for example docker search mysql
+
+// to search for containers list
+// # docker ps
+
+// # to run an container
+// # docker run imagename
+// docker run hello-world
+
